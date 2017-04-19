89b828d705416722db88417a1ab426e139ceeb33
jodabashyan39 <jodabash39@gmail.com>
2017-04-19 11:29:32 -0700

    last commit in this repo

--- proj_solution/proj/Source.cpp
+++ proj_solution/proj/Source.cpp
@@ -13,9 +13,31 @@ public:
 	Cosa() { std::cout << "Default" << std::endl; }
 
 	Cosa(int a) { std::cout << "Param" << std::endl; }
-private:
+protected:
 	int x;
 };
+//committed
+
+//do inheritance
+class ColorCosa : public Cosa
+{
+	std::string color;	//By default it's private
+	//In a class, if you forget to add "private" and add a field (int, string, etc)
+public:
+	ColorCosa(int a) 
+	{ 
+		x = 0; //to fix this error, you can make a getter function in Cosa, make them friends, OR make the private in Cosa protected
+		color = "no color";
+	}
+};
+
+//add a flavor
+class FlavorCosa : public Cosa
+{
+	std::string flavor;
+
+};
+
 int main()
 {
 	std::cout << "Hello world!" << std::endl;
