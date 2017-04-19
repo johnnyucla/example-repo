f74bbc1d972971faee1e253e93c9702d47a8db67
jodabashyan39 <jodabash39@gmail.com>
2017-04-19 11:16:55 -0700

    Create thing.

---
+++ proj_solution/proj/Source.cpp
@@ -0,0 +1,23 @@
+//thi is another example
+
+//sadly Kings are out of playoffs
+
+#include <iostream>
+#include <string>
+
+//add "thing"
+
+class Cosa
+{
+public:
+	Cosa() { std::cout << "Default" << std::endl; }
+
+	Cosa(int a) { std::cout << "Param" << std::endl; }
+private:
+	int x;
+};
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
